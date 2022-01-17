ac46c01e7669490d5adec633532bfc4da4425f39
Russ Carroll <info@rcarroll.dev>
2022-01-17 15:10:29 -0500

    Working on the user list output.

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Spinner from '../layout/Spinner'
+import UserItem from "./UserItem"
 
 function UserResults() {
+  const [users, setUsers ] = useState([])
+  const [loading, setLoading ] = useState(true)
+
   useEffect(() => {
     fetchUsers()
   }, []);
@@ -8,20 +13,27 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Athorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     });
 
     const data = await response.json();
 
-    console.log(data);
+    setUsers(data)
+    setLoading(false)
   };
 
-  return (
-    <div>
-      user results
-    </div>
-  )
+  if(!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-col-2">
+        {users.map((user) => 
+          <UserItem key={user.id} user={user} />
+        )}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserResults
