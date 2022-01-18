422e2dcf670db183c6e161e17653459c7a4bc9ae
Russ Carroll <info@rcarroll.dev>
2022-01-18 15:41:05 -0500

    Working on individual User profile page.

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,32 @@
+import { FaBullseye } from "react-icons/fa"
+
+const githubReducer = (state, action) => {
+  switch(action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      }
+    case 'GET_USER':
+      return {
+        ...state,
+        user: action.payload,
+        loading: FaBullseye
+      }
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true
+      }
+      case 'CLEAR_USERS':
+        return {
+          ...state,
+          users: [],
+        }
+    default:
+      return state
+  }
+}
+
+export default githubReducer
