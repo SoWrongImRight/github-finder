cbfaf201e58a433e17b925af16e93f0fb4802651
Russ Carroll <info@rcarroll.dev>
2022-01-20 22:41:02 -0500

    Finished project, User.jsx is not displaying.

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -41,7 +41,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (        
         <div>
-          <button onClick={() => dispatch({type: 'CLEAR_USER'})} className="btn btn-ghost btn-lg">
+          <button onClick={() => dispatch({type: 'CLEAR_USERS'})} className="btn btn-ghost btn-lg">
             Clear
           </button>
         </div>)}
